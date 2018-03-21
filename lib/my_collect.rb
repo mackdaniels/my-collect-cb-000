784ba4ecaaf05e8dcfc44b1824c9d055386a74d8
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-21 02:16:52 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
 
+def my_collect(array)
+  if block_given?
+    collection = []
+    i = 0
+    while i < array.length
+      yield(collection << array[i])
+      i = i + 1
+    end
+    collection
+  end
+    array
+end
