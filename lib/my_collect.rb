3238461086779d3fd230f2268d7fcf8e639d284f
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-21 02:40:32 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
     collection = []
     i = 0
     while i < array.length
-      yield(collection << array[i])
+      yield(collection << array[i].upcase)
       i = i + 1
     end
     collection
