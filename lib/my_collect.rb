2a0dc474b47c253bd2daad5c5ed2aabfda3c0950
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-21 02:48:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,10 +5,10 @@ def my_collect(array)
     collection = []
     i = 0
     while i < array.length
-      yield(collection << array[i])
+      collection << array[i]
       i = i + 1
     end
-    collection
+    yield collection
   end
     array
 end
